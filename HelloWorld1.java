5ac2f4ee1e0702cd74ba66decba97370ea8283ab
srinithig <39693577+srinithig@users.noreply.github.com>
2018-05-29 11:57:44 +0530

    Add files via upload

---
+++ HelloWorld1.java
@@ -0,0 +1,11 @@
+public class HelloWorld1
+{
+public static void main(String s[])
+{
+System.out.println("welcome to java training");
+byte one=80;
+byte two=15;
+int three=one+two;
+System.out.println(three);
+}
+}
